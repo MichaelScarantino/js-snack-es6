62e2295ea48303b80fb60576eaf886b873ed470c
MichaelScarantino <scarantino.michael@outlook.it>
2021-11-29 14:43:42 +0100

    Done Filter snack 3

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -4,14 +4,26 @@
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
 // creo le due variabili min e max 
+// const min = 2;
+// const max = 5;
+// inizializzo l'array vuoto
+// const newArray = [];
+// eseguo forEach dell'array iniziale, se il numero dell'indice e compreso tra min e max lo pusho nel nuovo array
+// myArray.forEach( (element, index, array) => {
+//     if( index > min && index < max ) {
+//         newArray.push(element);
+//     }
+// });
+// console.log(newArray);
+
+// --------------------------------------------------------------------
+
+// creo le due variabili min e max 
 const min = 2;
 const max = 5;
-// inizializzo l'array vuoto
-const newArray = [];
-// eseguo forEach dell'array iniziale, se il numero dell'indice e compreso tra min e max lo pusho nel nuovo array
-myArray.forEach( (element, index, array) => {
-    if( index > min && index < max ) {
-        newArray.push(element);
-    }
+
+// eseguo filter e ritorno gli elementi che sono compresi tra min e max all'interno dei newArray
+const newArray = myArray.filter( (element, index, array) => {
+    return index > min && index < max ;
 });
 console.log(newArray);
